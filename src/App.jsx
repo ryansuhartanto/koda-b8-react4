f41189a659cca9ac666e925ab5a2a23ab1590404
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-15 14:40:01 +0700

    feat: implement `getData`

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,37 @@ import { Outlet } from "react-router";
 // oxlint-disable-next-line no-unassigned-import
 import "#/style.css";
 
+/**
+ * @typedef Users
+ * @prop {number} id
+ * @prop {string} name
+ * @prop {string} username
+ * @prop {string} email
+ * @prop {object} address
+ * @prop {string} address.street
+ * @prop {string} address.suite
+ * @prop {string} address.city
+ * @prop {string} address.zipcode
+ * @prop {object} address.geo
+ * @prop {string} address.geo.lat
+ * @prop {string} address.geo.lng
+ * @prop {string} phone
+ * @prop {string} website
+ * @prop {object} company
+ * @prop {string} company.name
+ * @prop {string} company.catchPhrase
+ * @prop {string} company.bs
+ */
+
+/**
+ * @returns {Promise<Users[]>}
+ */
+async function getData() {
+	const req = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await req.json();
+	return data;
+}
+
 export default function Layout() {
 	return (
 		<>
